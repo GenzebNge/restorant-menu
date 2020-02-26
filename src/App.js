7a8fc7b5ad9e293685bb59b45d6827877e01b974
GenzebNge <>
2020-02-26 11:12:25 -0500

    some modification on the image of the menu component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import Menu from './components/MenuComponent'
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import './App.css';
+import {DISHES} from './shared/dishes'
 
 
 
 class App extends Component {
+  constructor(props){ 
+    super(props)
+    this.state ={
+      dishes:DISHES
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,7 +23,7 @@ class App extends Component {
             <NavbarBrand href="/"> Restorant Con fusion </NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
